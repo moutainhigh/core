d7b8dc80d48126fe6d012435f315618c5e691001
mafenglei <mafenglei@163.com>
2020-06-09 12:51:37 +0800

    jdk1.8-src

---
+++ jdk1.8-src/src/test/java/aom/gupaoedu/vip/design/VipApplicationTests.java
@@ -0,0 +1,13 @@
+package aom.gupaoedu.vip.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
