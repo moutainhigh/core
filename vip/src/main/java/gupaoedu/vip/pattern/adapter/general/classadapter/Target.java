5265bc8296b7fe6aac8d797c8b9ce9d877cdcdf1
mafenglei <mafenglei@163.com>
2020-06-15 12:05:09 +0800

    咕泡学院-P6架构师适配器桥接

---
+++ vip/src/main/java/gupaoedu/vip/pattern/adapter/general/classadapter/Target.java
@@ -0,0 +1,8 @@
+package gupaoedu.vip.pattern.adapter.general.classadapter;
+
+/**
+ * Created by Tom.
+ */
+public interface Target {
+    int request();
+}
---
+++ vip/src/main/java/gupaoedu/vip/pattern/adapter/general/objectadapter/Target.java
@@ -0,0 +1,8 @@
+package gupaoedu.vip.pattern.adapter.general.objectadapter;
+
+/**
+ * Created by Tom.
+ */
+public interface Target {
+    int request();
+}
