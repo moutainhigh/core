5dbdd198750ebd8027741a5e751ea2d988f96f5b
mafenglei <mafenglei@163.com>
2020-07-02 20:00:50 +0800

    delete

--- gupaoedu/spring-boot-demo/demo/src/main/java/com/gupaoedu/example/DemoClass.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.example;
-
-/**
- * 条件装配的类，已经在starter项目中的spring-autoconfigure-metadata.properties文件中进行了配置，
- * 这里如果删除此文件，启动就会报错，如果删除此文件，然后启动也不报差，就要把spring-autoconfigure-metadata.properties
- * 文件的配置注释掉
- */
-public class DemoClass {
-}
--- gupaoedu/spring-boot-demo/demo/src/main/java/com/gupaoedu/example/redis/demo03/GpSqlSessionFactory.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.example.redis.demo03;
-
-/**
- * 咕泡学院，只为更好的你
- * 咕泡学院-Mic: 2227324689
- * http://www.gupaoedu.com
- **/
-public class GpSqlSessionFactory {
-}
