d41b0570a15ea105169ae887775a35df054bfb01
mafenglei <mafenglei@163.com>
2020-07-18 15:51:41 +0800

    咕泡学院-P6架构师 NIO socket

--- vip/src/main/java/com/gupaoedu/vip/distributed/socket/nio/selectdemo/NewIOClient.java
+++ vip/src/main/java/com/gupaoedu/vip/distributed/socket/nio/selectdemo/NewIOClient.java
@@ -1,4 +1,4 @@
-package com.gupaoedu.vip.distributed.socket.nio;
+package com.gupaoedu.vip.distributed.socket.nio.selectdemo;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -10,24 +10,21 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * 咕泡学院，只为更好的你
- * 咕泡学院-Mic: 2227324689
- * http://www.gupaoedu.com
+ * NioClient
  **/
 public class NewIOClient {
-
 
     static Selector selector;
 
     public static void main(String[] args) {
         try {
-            selector = Selector.open();
+            selector = Selector.open(); //  打开多路复用器
             SocketChannel socketChannel = SocketChannel.open();
             socketChannel.configureBlocking(false);
             socketChannel.connect(new InetSocketAddress("localhost", 8080));
             socketChannel.register(selector, SelectionKey.OP_CONNECT); //连接事件
             while (true) {
-                selector.select();
+                selector.select(); //这里对于服务端不一样，这里只有一个客户端连接，这里连接只会建立一次，这样做只是为了更好的看到现象
                 Set<SelectionKey> selectionKeySet = selector.selectedKeys();
                 Iterator<SelectionKey> iterator = selectionKeySet.iterator();
                 while (iterator.hasNext()) {
@@ -35,7 +32,7 @@ public class NewIOClient {
                     iterator.remove();
                     if (key.isConnectable()) { //连接事件
                         handleConnect(key);
-                    } else if (key.isReadable()) {
+                    } else if (key.isReadable()) { // 可读事件
                         handleRead(key);
                     }
                 }
@@ -45,21 +42,25 @@ public class NewIOClient {
         }
     }
 
+
+    // 收到连接事件进行处理
     private static void handleConnect(SelectionKey selectionKey) throws IOException {
         SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
         if (socketChannel.isConnectionPending()) {
-            //
+            // 完成连接
             socketChannel.finishConnect();
         }
         socketChannel.configureBlocking(false);
         socketChannel.write(ByteBuffer.wrap("Hello Server,I'm NIo Client".getBytes()));
-        socketChannel.register(selector, SelectionKey.OP_READ); //
+        socketChannel.register(selector, SelectionKey.OP_READ); // 注册读事件到多路复用器
     }
 
+    // 读取数据
     private static void handleRead(SelectionKey selectionKey) throws IOException {
         SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-        //TODO
+        //TODO  这里读一次，如果怕缓冲区买的话，这里可以使用过while循环
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        // 这里读到的数据一定能都拿到这个通道，因为在连接进来的时候注册了读事件
         socketChannel.read(byteBuffer);
         System.out.println("client receive msg:" + new String(byteBuffer.array()));
 
