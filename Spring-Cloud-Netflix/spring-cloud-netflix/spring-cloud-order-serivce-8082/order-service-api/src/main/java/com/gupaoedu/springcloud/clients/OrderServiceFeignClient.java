5ab57f9f45785ac057460fe34a36cf2ec8d3d203
mafenglei <mafenglei@163.com>
2020-07-20 07:06:28 +0800

    咕泡学院-P6架构师 spring

--- Spring-Cloud-Netflix/spring-cloud-netflix/spring-cloud-order-serivce-8082/order-service-api/src/main/java/com/gupaoedu/springcloud/clients/OrderServiceFeignClient.java
+++ Spring-Cloud-Netflix/spring-cloud-netflix/spring-cloud-order-serivce-8082/order-service-api/src/main/java/com/gupaoedu/springcloud/clients/OrderServiceFeignClient.java
@@ -1,7 +1,9 @@
 package com.gupaoedu.springcloud.clients;
 
 import com.gupaoedu.springcloud.OrderService;
+import com.gupaoedu.springcloud.dto.OrderDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 
 /**
  * 咕泡学院，只为更好的你
@@ -9,7 +11,27 @@ import org.springframework.cloud.openfeign.FeignClient;
  * http://www.gupaoedu.com
  **/
 
-@FeignClient("order-service")
+//@FeignClient("order-service")
+//public interface OrderServiceFeignClient extends OrderService{
+//
+//}
+
+
+@FeignClient(value = "order-service",fallback = OrderServiceFeignClient.OrderServiceFeignClientFallback.class)
 public interface OrderServiceFeignClient extends OrderService{
 
+    @Component
+    class OrderServiceFeignClientFallback implements OrderServiceFeignClient{
+
+        @Override
+        public String orders() {
+            return "查询订单失败，请稍候重试";
+        }
+
+        @Override
+        public int insert(OrderDto dto) {
+            System.out.println("insert失败");
+            return -1;
+        }
+    }
 }
--- Spring-Cloud-Netflix/spring-cloud-netflix/spring-cloud-user-service-8081/user-service-provider/src/main/java/com/gupaoedu/example/userserviceprovider/UserServiceProviderApplication.java
+++ Spring-Cloud-Netflix/spring-cloud-netflix/spring-cloud-user-service-8081/user-service-provider/src/main/java/com/gupaoedu/example/userserviceprovider/UserServiceProviderApplication.java
@@ -2,14 +2,41 @@ package com.gupaoedu.example.userserviceprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.gupaoedu.example.controller")
+import java.util.HashMap;
+import java.util.Map;
+
+//@ComponentScan(basePackages = "com.gupaoedu.example.controller")
+//@SpringBootApplication
+//public class UserServiceProviderApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(UserServiceProviderApplication.class, args);
+//    }
+//
+//}
+
+
+@EnableCircuitBreaker
+@ComponentScan(basePackages = {
+        "com.gupaoedu.example.controller",
+        "com.gupaoedu.example.sourceloader",
+        "com.gupaoedu.example",
+        "com.gupaoedu.springcloud.clients"})
+@EnableFeignClients(basePackages = "com.gupaoedu.springcloud.clients")
 @SpringBootApplication
 public class UserServiceProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceProviderApplication.class, args);
+
+//        ConfigurableApplicationContext context=SpringApplication.run(UserServiceProviderApplication.class, args);
+        SpringApplication springApplication=new SpringApplication(UserServiceProviderApplication.class);
+        Map<String,Object> map=new HashMap<>();
+        map.put("key","value");
+        springApplication.setDefaultProperties(map);
+        springApplication.run(args);
     }
 
 }
