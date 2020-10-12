6b26290de5a7ddc38d9d8d1718800982e12f13e1
mafenglei <mafenglei@163.com>
2020-10-13 06:40:37 +0800

    springboot-security

---
+++ security/springboot_security/src/main/java/com/gupaoedu/security/GpVipSecurity1011Application.java
@@ -0,0 +1,13 @@
+package com.gupaoedu.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpVipSecurity1011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpVipSecurity1011Application.class, args);
+    }
+
+}
